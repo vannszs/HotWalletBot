10db8603a002110b1bac80d52d172bff50bd9e6a
vannszs <bevansatria@gmail.com>
2024-02-07 22:08:29 +0700

    v2.0 Stabele

--- main.py
+++ main.py
@@ -7,106 +7,104 @@ from selenium.webdriver.chrome.options import Options
 import time
 import re
 
-class HotWalletAutomation:
-    def __init__(self):
-        # Set opsi untuk mode anonim dan headless
-        self.chrome_options = Options()
-        self.chrome_options.add_argument("--incognito")
-        # Inisialisasi WebDriver dengan opsi yang telah diatur
-        self.driver = webdriver.Chrome(options=self.chrome_options)
-        self.wait = WebDriverWait(self.driver, 10)
-        self.error_count = 0
-        self.url = 'https://tgapp.herewallet.app/auth/import'
-        self.seed_file_path = "seed.txt"
-        self.load_seeds()
+session_path = "D:/selenimu2"
+os.makedirs(session_path, exist_ok=True)
 
-    def load_seeds(self):
-        # Membaca semua baris dari seed.txt
-        with open(self.seed_file_path, "r") as file:
-            self.seeds = [line.strip() for line in file]
+# Set opsi untuk menyimpan sesi
+chrome_options = Options()
+chrome_options.add_argument(f"user-data-dir={session_path}")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--headless")
 
-    def claim(self):
-        for seed_index, seed in enumerate(self.seeds):
-            try:
-                self.driver.get(self.url)
-                time.sleep(1)
+# Inisialisasi WebDriver dengan opsi yang telah diatur
+driver = webdriver.Chrome(options=chrome_options)
 
-                # Mengisi seed pada area input
-                seed_input = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/label/textarea')))
-                seed_input.click()
-                seed_input.send_keys(seed)
-                time.sleep(1)
+url = 'https://tgapp.herewallet.app/auth/import'
+time.sleep(1)
+wait = WebDriverWait(driver, 10)
 
-                # Menekan tombol untuk login
-                enter_seed_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[2]/button')))
-                enter_seed_button.click()
-                time.sleep(3)
+# Path untuk file seed
+seed_file_path = "seed.txt"
 
-                # Menekan tombol setelah login
-                enter_seed_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/button')))
-                enter_seed_button.click()
-                time.sleep(3)
 
-                # Loop tunggu hingga teks "Full" muncul pada elemen tertentu
-                while True:
-                    fulltext_xpath = '/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[1]/p'
-                    fulltext_element = self.driver.find_element(By.XPATH, fulltext_xpath)
-                    print("BERHASIL LOGIN")
+def Login(iseed):
+    err = 0
+    while True:
+        try:
+            driver.get(url)
+            seed_area = '/html/body/div[1]/div/div[1]/label/textarea'
+            seed_input = wait.until(EC.element_to_be_clickable((By.XPATH, seed_area)))
+            seed_input.click()
+            seed_input.send_keys(iseed)
+            time.sleep(1)
 
-                    if "Full" in fulltext_element.text:
-                        break
-                    else:
-                        self.click_element('/html/body/div[1]/div/div/div/div[3]/div[2]')
+            enter_seed_xpath = '//*[@id="root"]/div/div[2]/button'
+            enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, enter_seed_xpath)))
+            enter_seed_button.click()
+            time.sleep(3)
 
-                        waktu_xpath = '/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div/div[2]/div[1]/p[2]'
-                        total_waktu = self.get_total_wait_time(waktu_xpath)
+            enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/button')))
+            enter_seed_button.click()
+            time.sleep(3)
+            print("Berhasil Login")
+            enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div[3]/div[2]')))
+            enter_seed_button.click()
+            claim(iseed)
+            return
+        except:
+            if err > 5:
+                return
+            err +=1
+            continue
 
-                        print(f"Tunggu {total_waktu} Menit Untuk Claim Lagi")
-                        if total_waktu <30:
-                            break 
-                        time.sleep(total_waktu)
-                        print("Mencoba Claim Ulang")
+def claim(iseed):
+    err = 0
+    try:
+        while True:
+            waktu_xpath = '/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div/div[2]/div[1]/p[2]'
+            waktu_element = driver.find_element(By.XPATH, waktu_xpath)
+            waktu_text = waktu_element.text
+            # Gunakan regex untuk mencocokkan angka dan satuan waktu
+            matches = re.findall(r'(\d+)([hm])', waktu_text)
 
-                # Klik beberapa tombol berikutnya
-                self.claim_success(seed_index)
-                
-            except Exception as e:
-                print("Terjadi Error")
-                print("Mengulangi Lagi")
-                self.error_count += 1
-                if self.error_count > 5:
-                    print("Sepertinya anda mengalami error beberapa kali, silahkan coba hubungi @belugaa99 di discord dengan menyertakan screenshot error")
-                    self.error_count = 0
-                    continue
-                else:
-                    continue
+            # Hitung total waktu dalam menit
+            total_waktu = sum(int(value) * (60 if unit == 'h' else 1) for value, unit in matches)
+            if total_waktu < 30:
+                break
+            print(f"Belum Saatnya Claim. Tunggu {total_waktu} Menit")
+            time.sleep(total_waktu)
+            break
+        enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div/div[2]/div[2]/button')))
+        enter_seed_button.click()
+        print("BERHASIL CLAIM \n")
+        time.sleep(10)
+        return
+    except Exception as e:
+        # print(e)
+        if err == 0:
+            Login(iseed)
 
-    def click_element(self, xpath):
-        element = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
-        element.click()
 
-    def get_total_wait_time(self, xpath):
-        waktu_element = self.driver.find_element(By.XPATH, xpath)
-        waktu_text = waktu_element.text
+if os.path.exists(seed_file_path) and os.path.getsize(seed_file_path) > 0:
+    with open(seed_file_path, "r") as file:
+        seeds = [line.strip() for line in file]
+else:
+    seeds = input("Masukkan Seed (pisahkan dengan koma jika lebih dari satu): ").split(",")
+    with open(seed_file_path, "w") as file:
+        file.write("\n".join(seeds))
 
-        # Gunakan regex untuk mencocokkan angka dan satuan waktu
-        matches = re.findall(r'(\d+)([hm])', waktu_text)
-        # Hitung total waktu dalam menit
-        total_waktu = sum(int(value) * (60 if unit == 'h' else 1) for value, unit in matches)
-        return total_waktu
+iseed_index = 0
 
-    def claim_success(self, seed_index):
-        # Klik beberapa tombol berikutnya
-        self.click_element('/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div/div[2]/div[2]/button')
-        time.sleep(14)
-        print(f"Berhasil Klaim akun ke {seed_index}")
-
-    def close_browser(self):
-        # Tutup browser pada akhirnya
-        self.driver.quit()
-
-# Contoh penggunaan:
 while True:
-    automation = HotWalletAutomation()
-    automation.claim()
-    automation.close_browser()
+    print(f"Main Start on seed {iseed_index}")
+    try:
+        iseed = seeds[iseed_index]
+        Login(iseed)
+
+        iseed_index += 1
+        if iseed_index >= len(seeds):
+            iseed_index = 0
+    except:
+        iseed_index = 0
+
+     

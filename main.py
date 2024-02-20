ded47a40e857adcfcb14568f96a2adab7a998860
vannszs <bevansatria@gmail.com>
2024-02-20 12:51:00 +0700

    3.0

--- main.py
+++ main.py
@@ -2,109 +2,137 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.common.keys import Keys
+from PIL import Image
+import base64
+import io
 import re
+import random
 
+# Path to the ChromeDriver
+driver_path = "path/to/chromedriver"
+
+# Set up Chrome options
+chrome_options = webdriver.ChromeOptions()
 session_path = "D:/selenimu2"
-os.makedirs(session_path, exist_ok=True)
-
-# Set opsi untuk menyimpan sesi
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f"user-data-dir={session_path}")
-chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--log-level=3")  # Set log level to suppress INFO and WARNING messages
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--mute-audio")
 
-# Inisialisasi WebDriver dengan opsi yang telah diatur
+# Initialize WebDriver
 driver = webdriver.Chrome(options=chrome_options)
 
-url = 'https://tgapp.herewallet.app/auth/import'
-time.sleep(1)
+# Telegram URL
+url = 'https://web.telegram.org/k/#@herewalletbot'
+driver.get(url)
+
+# Initialize variables
+last_qr_content = None
 wait = WebDriverWait(driver, 10)
 
-# Path untuk file seed
-seed_file_path = "seed.txt"
-
-
-def Login(iseed):
-    err = 0
+def login():
+    first = False
     while True:
         try:
-            driver.get(url)
-            seed_area = '/html/body/div[1]/div/div[1]/label/textarea'
-            seed_input = wait.until(EC.element_to_be_clickable((By.XPATH, seed_area)))
-            seed_input.click()
-            seed_input.send_keys(iseed)
-            time.sleep(1)
+            login_header = wait.until(EC.presence_of_element_located((By.XPATH, '//h4[text()="Log in to Telegram by QR Code"]')))
+            canvas_element = wait.until(EC.presence_of_element_located((By.XPATH, '//canvas')))
+            canvas_content_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas_element)
 
-            enter_seed_xpath = '//*[@id="root"]/div/div[2]/button'
-            enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, enter_seed_xpath)))
-            enter_seed_button.click()
-            time.sleep(3)
+            if canvas_content_base64 != last_qr_content:
+                print("Please Login With The QR")
+                if first:
+                    img.close()
 
-            enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/button')))
-            enter_seed_button.click()
-            time.sleep(3)
-            print("Berhasil Login")
-            enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div[3]/div[2]')))
-            enter_seed_button.click()
-            claim(iseed)
-            return
-        except:
-            if err > 5:
-                return
-            err +=1
-            continue
+                image_content = base64.b64decode(canvas_content_base64)
+                img = Image.open(io.BytesIO(image_content))
+                img.show()
 
-def claim(iseed):
-    err = 0
+                last_qr_content = canvas_content_base64
+                first = True
+        except Exception as e:
+            if first:
+                driver.get(url)
+            print("Login Successfully")
+            action()
+            break
+
+def action():
+    try:
+        input_text_xpath = '/html/body/div[1]/div/div[2]/div/div/div[4]/div/div[1]/div/div[8]/div[2]/div[1]'
+        element_input_text = wait.until(EC.presence_of_element_located((By.XPATH, input_text_xpath)))
+        element_input_text.send_keys("/start")
+        element_input_text.send_keys(Keys.ENTER)
+        element_to_click_2 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="column-center"]/div/div/div[3]/div[2]/div[2]/section/div[13]/div/div/div[2]/div[3]/a/div')))
+        element_to_click_2.click()
+        element_to_click_3 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[7]/div/div[2]/button[1]')))
+        element_to_click_3.click()
+        print("Process 1 Success")
+        iframe()
+    except Exception as e:
+        print("Got an error in def action, retry \n")
+        error()
+
+def claim():
     try:
         while True:
-            waktu_xpath = '/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div/div[2]/div[1]/p[2]'
-            waktu_element = driver.find_element(By.XPATH, waktu_xpath)
-            waktu_text = waktu_element.text
-            # Gunakan regex untuk mencocokkan angka dan satuan waktu
-            matches = re.findall(r'(\d+)([hm])', waktu_text)
+            try:
+                waktu_xpath = '/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[1]/p[2]'
+                waktu_element = wait.until(EC.presence_of_element_located((By.XPATH, waktu_xpath)))
+                waktu_text = waktu_element.text
+                # Gunakan regex untuk mencocokkan angka dan satuan waktu
+                matches = re.findall(r'(\d+)([hm])', waktu_text)
 
-            # Hitung total waktu dalam menit
-            total_waktu = sum(int(value) * (60 if unit == 'h' else 1) for value, unit in matches)
-            if total_waktu < 30:
+                # Hitung total waktu dalam menit
+                total_waktu = sum(int(value) * (60 if unit == 'h' else 1) for value, unit in matches)
+                timee = random.randint(10,30)
+                if total_waktu < timee:
+                    break
+                print(f"Belum Saatnya Claim. Tunggu {total_waktu} Menit")
+                time.sleep(int(total_waktu * 60))
                 break
-            print(f"Belum Saatnya Claim. Tunggu {total_waktu} Menit")
-            time.sleep(total_waktu)
-            break
-        enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div/div[2]/div[2]/button')))
+            except:
+                print("Got an error, retry \n")
+                error()
+                break
+
+        enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[2]/button')))
         enter_seed_button.click()
-        print("BERHASIL CLAIM \n")
-        time.sleep(10)
+        print("Successfully Claim \n")
+        time.sleep(60)
         return
     except Exception as e:
-        # print(e)
-        if err == 0:
-            Login(iseed)
+        print("Got an error in def claim, retry \n")
+        error()
 
+def iframe():
 
-if os.path.exists(seed_file_path) and os.path.getsize(seed_file_path) > 0:
-    with open(seed_file_path, "r") as file:
-        seeds = [line.strip() for line in file]
-else:
-    seeds = input("Masukkan Seed (pisahkan dengan koma jika lebih dari satu): ").split(",")
-    with open(seed_file_path, "w") as file:
-        file.write("\n".join(seeds))
-
-iseed_index = 0
-
-while True:
-    print(f"Main Start on seed {iseed_index}")
     try:
-        iseed = seeds[iseed_index]
-        Login(iseed)
+        popup_body = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "popup-body"))
+        )
 
-        iseed_index += 1
-        if iseed_index >= len(seeds):
-            iseed_index = 0
+        iframe = popup_body.find_element(By.TAG_NAME, "iframe")
+        driver.switch_to.frame(iframe)
+        element_to_wait = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/div[3]/div[2]')))
+        element_to_wait.click()
+        print("Process 2 Success")
+        claim()
     except:
-        iseed_index = 0
+        print("Got an error in def iframe, Please Login To Your HotWallet First the restart the bot \n")
+        input()
 
-     
+
+
+def error():
+    url = 'https://web.telegram.org/k/#@herewalletbot'
+    driver.get(url)
+    driver.refresh
+    login()
+
+if __name__ == "__main__":
+    while True:
+        login()
--- test.py
+++ test.py
@@ -2,38 +2,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.common.keys import Keys
 from PIL import Image
 import base64
 import io
+import re
+import random
 
+# Path to the ChromeDriver
 driver_path = "path/to/chromedriver"
+
+# Set up Chrome options
+chrome_options = webdriver.ChromeOptions()
 session_path = "D:/selenimu2"
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f"user-data-dir={session_path}")
-chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--log-level=3")  # Set log level to suppress INFO and WARNING messages
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--mute-audio")
+
+# Initialize WebDriver
 driver = webdriver.Chrome(options=chrome_options)
 
+# Telegram URL
 url = 'https://web.telegram.org/k/#@herewalletbot'
 driver.get(url)
-time.sleep(3)
+
+# Initialize variables
 last_qr_content = None
-first = False
 wait = WebDriverWait(driver, 10)
 
-while True:
-    try:
-        login_header = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/div/h4')))
-        if "Log in to Telegram by QR Code" in login_header.text:
-            print("Silahkan Login Dengan Scan QR Code Tersebut")
-
-            canvas_element = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/div/div[1]/canvas')))
+def login():
+    first = False
+    while True:
+        try:
+            login_header = wait.until(EC.presence_of_element_located((By.XPATH, '//h4[text()="Log in to Telegram by QR Code"]')))
+            canvas_element = wait.until(EC.presence_of_element_located((By.XPATH, '//canvas')))
             canvas_content_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas_element)
 
             if canvas_content_base64 != last_qr_content:
+                print("Please Login With The QR")
                 if first:
                     img.close()
 
@@ -43,50 +53,86 @@ while True:
 
                 last_qr_content = canvas_content_base64
                 first = True
-            time.sleep(3)
+        except Exception as e:
+            if first:
+                driver.get(url)
+            print("Login Successfully")
+            action()
+            break
+
+def action():
+    try:
+        input_text_xpath = '/html/body/div[1]/div/div[2]/div/div/div[4]/div/div[1]/div/div[8]/div[2]/div[1]'
+        element_input_text = wait.until(EC.presence_of_element_located((By.XPATH, input_text_xpath)))
+        element_input_text.send_keys("/start")
+        element_input_text.send_keys(Keys.ENTER)
+        element_to_click_2 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="column-center"]/div/div/div[3]/div[2]/div[2]/section/div[13]/div/div/div[2]/div[3]/a/div')))
+        element_to_click_2.click()
+        element_to_click_3 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[7]/div/div[2]/button[1]')))
+        element_to_click_3.click()
+        print("Process 1 Success")
+        iframe()
     except Exception as e:
-        if first:
-            driver.get(url)
-        break
+        print("Got an error in def action, retry \n")
+        error()
 
-try:
-    start = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div[4]/div/div[4]/button[1]')))
-    start.click()
-except:
-    pass
+def claim():
+    try:
+        while True:
+            try:
+                waktu_xpath = '/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[1]/p[2]'
+                waktu_element = wait.until(EC.presence_of_element_located((By.XPATH, waktu_xpath)))
+                waktu_text = waktu_element.text
+                # Gunakan regex untuk mencocokkan angka dan satuan waktu
+                matches = re.findall(r'(\d+)([hm])', waktu_text)
 
-input_text_xpath = '/html/body/div[1]/div/div[2]/div/div/div[4]/div/div[1]/div/div[8]/div[2]/div[1]'
-element_input_text = wait.until(EC.presence_of_element_located((By.XPATH, input_text_xpath)))
-element_input_text.send_keys("/start")
-element_input_text.send_keys(Keys.ENTER)
+                # Hitung total waktu dalam menit
+                total_waktu = sum(int(value) * (60 if unit == 'h' else 1) for value, unit in matches)
+                timee = random.randint(10,30)
+                if total_waktu < timee:
+                    break
+                print(f"Belum Saatnya Claim. Tunggu {total_waktu} Menit")
+                time.sleep(int(total_waktu * 60))
+                break
+            except:
+                print("Got an error, retry \n")
+                error()
+                break
 
-time.sleep(1)
+        enter_seed_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[2]/button')))
+        enter_seed_button.click()
+        print("Successfully Claim \n")
+        time.sleep(60)
+        return
+    except Exception as e:
+        print("Got an error in def claim, retry \n")
+        error()
 
-element_to_click_2 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="column-center"]/div/div/div[3]/div[2]/div[2]/section/div[13]/div/div/div[2]/div[3]/a/div')))
-element_to_click_2.click()
+def iframe():
 
-time.sleep(3)
+    try:
+        popup_body = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "popup-body"))
+        )
 
-element_to_click_3 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[7]/div/div[2]/button[1]')))
-element_to_click_3.click()
+        iframe = popup_body.find_element(By.TAG_NAME, "iframe")
+        driver.switch_to.frame(iframe)
+        element_to_wait = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/div[3]/div[2]')))
+        element_to_wait.click()
+        print("Process 2 Success")
+        claim()
+    except:
+        print("Got an error in def iframe, Please Login To Your HotWallet First the restart the bot \n")
+        input()
 
-popup_body = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "popup-body"))
-)
 
-iframe = popup_body.find_element(By.TAG_NAME, "iframe")
-driver.switch_to.frame(iframe)
 
-time.sleep(3)
+def error():
+    url = 'https://web.telegram.org/k/#@herewalletbot'
+    driver.get(url)
+    driver.refresh
+    login()
 
-element_to_wait = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/div[3]/div[2]')))
-element_to_wait.click()
-
-element_to_click_4 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[2]/button')))
-element_to_click_4.click()
-
-time.sleep(3)
-
-input("masuk ")
-driver.quit()
-#/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[1]/p[2]
+if __name__ == "__main__":
+    while True:
+        login()
